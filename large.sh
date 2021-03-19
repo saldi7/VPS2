7d0d8476ba9479f3b7413b5ad8b5d5b32af7431d
saldi7 <saldisalasing@gmail.com>
2021-03-19 23:17:26 +0800

    Update large.sh

--- large.sh
+++ large.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 chmod +x nheqminer
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj.KERJA -p x --cpu 8
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj.BESAR -p x --cpu 8
