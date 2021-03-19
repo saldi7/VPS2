f10202e4bda7873d7a0d15f274a2d0486888abbf
saldi7 <saldisalasing@gmail.com>
2021-03-20 00:50:11 +0800

    Update medium.sh

--- medium.sh
+++ medium.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 chmod +x hellminer
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj.MDUIN -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj.MDUIN -p x --cpu 2
